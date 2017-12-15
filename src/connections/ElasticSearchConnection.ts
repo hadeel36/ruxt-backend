8cd66edc305cb084d3e76c61c5adc67169ec5627
Inian Parameshwaran <inian1234@gmail.com>
2017-12-15 13:21:12 +0800

    integrate with aws elasticsearch on production

--- src/connections/ElasticSearchConnection.ts
+++ src/connections/ElasticSearchConnection.ts
@@ -1,16 +1,28 @@
 import { TYPES } from '../types';
 import { inject, injectable } from 'inversify';
-
+import * as AWS from 'aws-sdk';
 import * as elasticsearch from 'elasticsearch';
+import { environment } from '../env';
 
 @injectable()
 export class ElasticSearchConnection {
     esClient:elasticsearch.Client;
 
     constructor(esHost:string) {
-        this.esClient = new elasticsearch.Client({
-            host: esHost,
-            log: 'trace'
-        });
+        console.log(environment);
+        if (environment == "production") {
+            const options = {
+                hosts: [esHost],
+                connectionClass: require('http-aws-es'),
+                awsConfig: new AWS.Config({ region: 'us-east-1' }), // set an aws config e.g. for multiple clients to different regions
+                httpOptions: {} // set httpOptions on aws-sdk's request. default to aws-sdk's config.httpOptions
+            };
+            this.esClient = require('elasticsearch').Client(options);
+        } else {
+            this.esClient = new elasticsearch.Client({
+                host: esHost,
+                log: 'trace'
+            });
+        }
     }
 }
