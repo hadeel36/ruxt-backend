217968d1d95bf5df10047fa6275afd9f8ed084b1
Inian Parameshwaran <inian1234@gmail.com>
2018-03-21 18:24:20 +0800

    redis deleteall

--- src/scripts/env.js
+++ src/scripts/env.js
@@ -4,3 +4,5 @@ exports.googleApplicationCredentials = process.env.GOOGLE_APPLICATION_CREDENTIAL
 exports.BigQueryProjectId = process.env.PROJECT_ID || 'chrome-ux-report-185710';
 exports.dataSetName = process.env.dataSetName || 'chrome-ux-report.all.201802';
 exports.ESHost = process.env.ESHOST || 'localhost:9200';
+exports.RedisHost = process.env.REDIS_HOST || 'localhost',
+exports.RedisPort = isNaN(parseInt(process.env.REDIS_PORT)) ? 6379 : parseInt(process.env.REDIS_PORT);
