732b2ba8e007dc9e9741545d342a491c4d908c77
Inian Parameshwaran <inian1234@gmail.com>
2018-03-25 16:45:42 +0800

    changing to new BQ dataset

--- src/env.ts
+++ src/env.ts
@@ -5,6 +5,7 @@ export interface IEnviroment {
     RedisHost: string;
     RedisPort: number;
     BigQueryProjectId: string;
+    BigQueryDataset: string;
     BaseUri: string;
     OriginCount: number;
     environment: string;
@@ -18,6 +19,7 @@ export const env:IEnviroment = {
     RedisHost: process.env.REDIS_HOST || 'localhost',
     RedisPort: isNaN(parseInt(process.env.REDIS_PORT)) ? 6379 : parseInt(process.env.REDIS_PORT),
     BigQueryProjectId: process.env.PROJECT_ID || 'chrome-ux-report-185710',
+    BigQueryDataset: 'chrome-ux-report.all.201802',
     BaseUri: process.env.BASE_URI || null,
     OriginCount: isNaN(parseInt(process.env.ORIGIN_COUNT)) ? 10000 : parseInt(process.env.ORIGIN_COUNT),
     environment: process.env.NODE_ENV || 'development',
